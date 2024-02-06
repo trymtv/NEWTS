a40511ba5c3451331bd458f9f41d39b625e73060
TrymT <tiltrym@gmail.com>
2024-02-06 15:07:59 +0100

    add: choose top topics and words

--- ldamodel.py
+++ ldamodel.py
@@ -6,16 +6,37 @@ import numpy as np
 
 class TopicModel(object):
     """Reads a pretrained LDA model trained using the Gensim library"""
-    def __init__(self, model_path, dict_path):
+    def __init__(self, model_path, dict_path, num_topics = None, vocab_size=None):
         self.topic_model, self.topic_model_dictionary = self._load_pretrained_model(model_path, dict_path)
         self.topic_model.minimum_phi_value = 0.01
         self.topic_model.per_word_topics = False
-        self.word_topic_prob = None
+        self.vocab_size = vocab_size
+        self._word_topic_prob = None
+        self.num_topics = self.topic_model.num_topics if num_topics is None else num_topics
+        self._top_topic_to_topic = None
+        self._topic_to_top_topic = None
+        self._top_word_to_word = None
+        self._word_to_top_word = None
 
     def get_word_topic_prob(self):
-        if self.word_topic_prob is None:
-            self.word_topic_prob = self.topic_model.get_topics()
-        return self.word_topic_prob
+        if self._word_topic_prob is None:
+            if self.vocab_size is None:
+                self._word_topic_prob = self.topic_model.get_topics()
+            else:
+                self._word_topic_prob = self._gen_new_word_topic_prob()
+        return self._word_topic_prob
+    
+    def _gen_new_word_topic_prob(self):
+        top_topics_with_words = self.topic_model.show_topics(num_topics = self.num_topics, num_words = self.vocab_size, formatted=False)
+        top_topics = sorted([topic_id for topic_id , _ in top_topics_with_words])
+        top_words = {self.topic_model_dictionary.token2id[word] for _, word_list in top_topics_with_words for word, _ in word_list}
+        top_words = sorted(list(top_words))
+        self._top_topic_to_topic = {i: topic for i, topic in enumerate(top_topics)}
+        self._top_word_to_word = {i: word for i, word in enumerate(top_words)}
+        self._topic_to_top_topic = {topic: i for i, topic in self._top_topic_to_topic.items()}
+        self._word_to_top_word = {word: i for i, word in self._top_word_to_word.items()}
+        all_topics_to_all_words = self.topic_model.get_topics()
+        return all_topics_to_all_words[np.ix_(top_topics, top_words)]
     
     def _load_pretrained_model (self, model_path, dict_path):
         lda = LdaModel.load(model_path, mmap = 'r')
@@ -24,7 +45,7 @@ class TopicModel(object):
         print("Loaded dictionary.")
         return lda, dictionary
     
-    def doc2topics (self, doc):
+    def doc2topics(self, doc):
         vec_bow = self.topic_model_dictionary.doc2bow(doc.split())
         perDocTopics = self.topic_model.get_document_topics(vec_bow, minimum_probability=0)
         return perDocTopics
@@ -32,8 +53,9 @@ class TopicModel(object):
     def doc_2_ids(self, doc):
         return [self.topic_model_dictionary.token2id.get(token, -1) for token in doc.split()]
 
-    def tau_calc(self, article, time=False):
+    def tau_calc(self, article):
         W = self.get_word_topic_prob()
-        T = np.mat([topic_prob for topic_id, topic_prob in self.doc2topics(article)])
+        relevant_topics = set(self._topic_to_top_topic.keys())
+        T = np.mat([topic_prob for topic_id, topic_prob in self.doc2topics(article) if topic_id in relevant_topics])
         tau_d = T*W
         return tau_d
--- test.py
+++ test.py
@@ -6,7 +6,14 @@ import time
 def get_token_weights(token_ids, token_weights, default_weight=1e-9):
     return [token_weights[0, token_id] if token_id != -1 else default_weight for token_id in token_ids]
 
-model = TopicModel(model_path='./testmodel/lda.model', dict_path='./testmodel/dictionary.dic')
-id = model.topic_model_dictionary.token2id['potato']
+model = TopicModel(model_path='./testmodel/lda.model', dict_path='./testmodel/dictionary.dic', vocab_size=10, num_topics=10)
+model.get_word_topic_prob()
+id = model._word_to_top_word[model.topic_model_dictionary.token2id['money']]
 topic_id = model.get_word_topic_prob()[:, id].argmax()
-print(model.topic_model.show_topic(topic_id))
+print(model.topic_model.show_topic(model._top_topic_to_topic[topic_id]))
+test_pd = read_test()
+test_article = test_pd.article.iloc[0]
+test_tau = model.tau_calc(test_article)
+from pprint import pprint
+pprint(model._word_topic_prob)
+print(test_tau)
