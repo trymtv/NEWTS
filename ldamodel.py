ef28336bee0ac259a949510f4672cf5da39c3a2e
TrymT <tiltrym@gmail.com>
2024-02-07 16:01:51 +0100

    add: bow and token weights calc

--- ldamodel.py
+++ ldamodel.py
@@ -1,36 +1,41 @@
 from gensim.models.ldamodel import LdaModel
 from gensim.corpora import Dictionary
 import numpy as np
+from collections import defaultdict 
+import torch
 
 # from https://github.com/ali-bahrainian/CATS/blob/main/data.py
 
 class TopicModel(object):
     """Reads a pretrained LDA model trained using the Gensim library"""
-    def __init__(self, model_path, dict_path, num_topics = None, vocab_size=None):
+    def __init__(self, model_path, dict_path, num_topics = None, vocab_per_topic_size=None, oov_value=1e-8):
         self.topic_model, self.topic_model_dictionary = self._load_pretrained_model(model_path, dict_path)
         self.topic_model.minimum_phi_value = 0.01
         self.topic_model.per_word_topics = False
-        self.vocab_size = vocab_size
+        self.vocab_per_topic_size = vocab_per_topic_size
+        self.vocab_size = None
         self._word_topic_prob = None
         self.num_topics = self.topic_model.num_topics if num_topics is None else num_topics
         self._top_topic_to_topic = None
         self._topic_to_top_topic = None
         self._top_word_to_word = None
         self._word_to_top_word = None
+        self._oov_value = oov_value
 
     def get_word_topic_prob(self):
         if self._word_topic_prob is None:
-            if self.vocab_size is None:
+            if self.vocab_per_topic_size is None:
                 self._word_topic_prob = self.topic_model.get_topics()
             else:
                 self._word_topic_prob = self._gen_new_word_topic_prob()
         return self._word_topic_prob
     
     def _gen_new_word_topic_prob(self):
-        top_topics_with_words = self.topic_model.show_topics(num_topics = self.num_topics, num_words = self.vocab_size, formatted=False)
+        top_topics_with_words = self.topic_model.show_topics(num_topics = self.num_topics, num_words = self.vocab_per_topic_size, formatted=False)
         top_topics = sorted([topic_id for topic_id , _ in top_topics_with_words])
         top_words = {self.topic_model_dictionary.token2id[word] for _, word_list in top_topics_with_words for word, _ in word_list}
         top_words = sorted(list(top_words))
+        self.vocab_size = len(top_words)
         self._top_topic_to_topic = {i: topic for i, topic in enumerate(top_topics)}
         self._top_word_to_word = {i: word for i, word in enumerate(top_words)}
         self._topic_to_top_topic = {topic: i for i, topic in self._top_topic_to_topic.items()}
@@ -45,17 +50,43 @@ class TopicModel(object):
         print("Loaded dictionary.")
         return lda, dictionary
     
-    def doc2topics(self, doc):
-        vec_bow = self.topic_model_dictionary.doc2bow(doc.split())
+    def doc2topics(self, tokens):
+        vec_bow = self.topic_model_dictionary.doc2bow(tokens)
         perDocTopics = self.topic_model.get_document_topics(vec_bow, minimum_probability=0)
         return perDocTopics
 
-    def doc_2_ids(self, doc):
-        return [self.topic_model_dictionary.token2id.get(token, -1) for token in doc.split()]
+    def token2id(self, token):
+        return self._word_to_top_word.get(self.topic_model_dictionary.token2id.get(token, -1), -1)
 
-    def tau_calc(self, article):
+    def id2token(self, id):
+        return self.topic_model_dictionary.id2token.get(self._top_word_to_word.get(id, -1), -1)
+
+    def get_token_weights(self, tokens):
+        tau = self.tau_calc(tokens)
+        token_weights = [tau[self.token2id(token)] for token in tokens]
+        return tokens, token_weights
+    
+    def bow_dict(self, tokens):
+        bow_dict = defaultdict(lambda: 0)
+        for token in tokens:
+            bow_dict[self.token2id(token)] += 1
+        return bow_dict
+    
+    def bow_tensor(self, tokens):
+        bow_dict = self.bow_dict(tokens)
+        bow_dict.pop(-1)
+        word_indices = list(bow_dict.keys())
+        word_counts =  list(bow_dict.values())
+        indices_tensor = torch.tensor(word_indices)
+        counts_tensor = torch.tensor(word_counts)
+        return torch.sparse_coo_tensor(indices_tensor.view(1, -1), counts_tensor, size=(self.vocab_size,))
+
+    def doc_2_ids(self, doc):
+        return [self.token2id.get(token, -1) for token in doc.split()]
+
+    def tau_calc(self, tokens):
         W = self.get_word_topic_prob()
         relevant_topics = set(self._topic_to_top_topic.keys())
-        T = np.mat([topic_prob for topic_id, topic_prob in self.doc2topics(article) if topic_id in relevant_topics])
+        T = np.mat([topic_prob for topic_id, topic_prob in self.doc2topics(tokens) if topic_id in relevant_topics])
         tau_d = T*W
-        return tau_d
+        return np.append(np.ravel(tau_d), self._oov_value)
--- test.py
+++ test.py
@@ -6,14 +6,20 @@ import time
 def get_token_weights(token_ids, token_weights, default_weight=1e-9):
     return [token_weights[0, token_id] if token_id != -1 else default_weight for token_id in token_ids]
 
-model = TopicModel(model_path='./testmodel/lda.model', dict_path='./testmodel/dictionary.dic', vocab_size=10, num_topics=10)
+model = TopicModel(model_path='./testmodel/lda.model', dict_path='./testmodel/dictionary.dic', vocab_per_topic_size=1000, num_topics=100)
 model.get_word_topic_prob()
-id = model._word_to_top_word[model.topic_model_dictionary.token2id['money']]
+id = model.token2id('lady')
 topic_id = model.get_word_topic_prob()[:, id].argmax()
+top_word_ids = np.argsort(-model.get_word_topic_prob()[topic_id, :])[:10]
+print([model.id2token(top_id) for top_id in top_word_ids])
 print(model.topic_model.show_topic(model._top_topic_to_topic[topic_id]))
 test_pd = read_test()
 test_article = test_pd.article.iloc[0]
-test_tau = model.tau_calc(test_article)
+test_tokens = test_article.split()
+test_tau = model.tau_calc(test_article.split())
 from pprint import pprint
 pprint(model._word_topic_prob)
 print(test_tau)
+print(model.bow_dict(test_tokens))
+print(model.bow_tensor(test_tokens)[676])
+print(model.get_token_weights(test_tokens))
