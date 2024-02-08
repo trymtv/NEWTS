dd766f53724a5114da826919bd6b8f983d2002a7
TrymT <tiltrym@gmail.com>
2024-02-08 20:28:59 +0100

    format and add: new taur calculation funciton

--- ldamodel.py
+++ ldamodel.py
@@ -1,101 +1,157 @@
 from gensim.models.ldamodel import LdaModel
 from gensim.corpora import Dictionary
 import numpy as np
-from collections import defaultdict 
+from collections import defaultdict
 import torch
 from sklearn.feature_extraction.text import CountVectorizer
 
 # from https://github.com/ali-bahrainian/CATS/blob/main/data.py
 
+
 class TopicModel(object):
     """Reads a pretrained LDA model trained using the Gensim library"""
-    def __init__(self, model_path, dict_path, num_topics = None, vocab_per_topic_size=None, oov_value=1e-8):
-        self.topic_model, self.topic_model_dictionary = self._load_pretrained_model(model_path, dict_path)
+
+    def __init__(
+        self,
+        model_path,
+        dict_path,
+        num_topics=None,
+        vocab_per_topic_size=None,
+        oov_value=1e-8,
+    ):
+        self.topic_model, self.topic_model_dictionary = self._load_pretrained_model(
+            model_path, dict_path
+        )
         self.topic_model.minimum_phi_value = 0.01
         self.topic_model.per_word_topics = False
-        self.vocab_per_topic_size = vocab_per_topic_size
+
         self.vocab_size = None
         self._word_topic_prob = None
-        self.num_topics = self.topic_model.num_topics if num_topics is None else num_topics
-        self._top_topic_to_topic = None
-        self._topic_to_top_topic = None
-        self._top_word_to_word = None
-        self._word_to_top_word = None
+        self.num_topics = (
+            self.topic_model.num_topics if num_topics is None else num_topics
+        )
+        self._new_topic_to_old_topic = None
+        self._old_topic_to_new_topic = None
+        self._new_word_to_old_word = None
+        self._old_word_to_new_word = None
+        self.old_relevant_topics = None
         self._oov_value = oov_value
+        self.vocab_per_topic_size = vocab_per_topic_size
 
-    def get_word_topic_prob(self):
-        if self._word_topic_prob is None:
-            if self.vocab_per_topic_size is None:
-                self._word_topic_prob = self.topic_model.get_topics()
-            else:
-                self._word_topic_prob = self._gen_new_word_topic_prob()
-        return self._word_topic_prob
-    
+        self.word_topic_prob = self._gen_new_word_topic_prob()
+
     def _gen_new_word_topic_prob(self):
-        top_topics_with_words = self.topic_model.show_topics(num_topics = self.num_topics, num_words = self.vocab_per_topic_size, formatted=False)
-        top_topics = sorted([topic_id for topic_id , _ in top_topics_with_words])
-        top_words = {self.topic_model_dictionary.token2id[word] for _, word_list in top_topics_with_words for word, _ in word_list}
-        top_words = sorted(list(top_words))
-        self.vocab_size = len(top_words)
-        self._top_topic_to_topic = {i: topic for i, topic in enumerate(top_topics)}
-        self._top_word_to_word = {i: word for i, word in enumerate(top_words)}
-        self._topic_to_top_topic = {topic: i for i, topic in self._top_topic_to_topic.items()}
-        self._word_to_top_word = {word: i for i, word in self._top_word_to_word.items()}
+        print(f"Generating new word to topic probability matrix.")
+        top_topics_with_words = self.topic_model.show_topics(
+            num_topics=self.num_topics,
+            num_words=self.vocab_per_topic_size,
+            formatted=False,
+        )
+        top_topics = sorted([topic_id for topic_id, _ in top_topics_with_words])
+        top_word_ids = {
+            self.topic_model_dictionary.token2id[word]
+            for _, word_list in top_topics_with_words
+            for word, _ in word_list
+        }
+        top_word_ids = sorted(list(top_word_ids))
+        self.vocab_size = len(top_word_ids)
+
+        self._new_topic_to_old_topic = {i: topic for i, topic in enumerate(top_topics)}
+        self._new_word_to_old_word = {i: word for i, word in enumerate(top_word_ids)}
+        self._old_topic_to_new_topic = {
+            topic: i for i, topic in self._new_topic_to_old_topic.items()
+        }
+        self._old_word_to_new_word = {word: i for i, word in self._new_word_to_old_word.items()}
+
+        self.old_relevant_topics = sorted(list(self._old_topic_to_new_topic.keys()))
+
         all_topics_to_all_words = self.topic_model.get_topics()
-        return all_topics_to_all_words[np.ix_(top_topics, top_words)]
-    
-    def _load_pretrained_model (self, model_path, dict_path):
-        lda = LdaModel.load(model_path, mmap = 'r')
+        new_topics_to_new_words = all_topics_to_all_words[np.ix_(top_topics, top_word_ids)]
+        return new_topics_to_new_words
+
+    def _load_pretrained_model(self, model_path, dict_path):
+        lda = LdaModel.load(model_path, mmap="r")
         print("Loaded the LDA model.")
-        dictionary = Dictionary.load(dict_path, mmap = 'r')
+        dictionary = Dictionary.load(dict_path, mmap="r")
         print("Loaded dictionary.")
         return lda, dictionary
-    
+
     def doc2topics(self, tokens):
         vec_bow = self.topic_model_dictionary.doc2bow(tokens)
-        perDocTopics = self.topic_model.get_document_topics(vec_bow, minimum_probability=0)
+        perDocTopics = self.topic_model.get_document_topics(
+            vec_bow, minimum_probability=0
+        )
         return perDocTopics
 
     def token2id(self, token):
-        return self._word_to_top_word.get(self.topic_model_dictionary.token2id.get(token, -1), -1)
+        return self._old_word_to_new_word.get(
+            self.topic_model_dictionary.token2id.get(token, -1), -1
+        )
 
     def id2token(self, id):
-        return self.topic_model_dictionary.id2token.get(self._top_word_to_word.get(id, -1), -1)
+        return self.topic_model_dictionary.id2token.get(
+            self._new_word_to_old_word.get(id, -1), -1
+        )
 
     def get_token_weights(self, tokens):
         tau = self.tau_calc(tokens)
         token_weights = [tau[self.token2id(token)] for token in tokens]
         return tokens, token_weights
-    
+
     def bow_dict(self, tokens):
         bow_dict = defaultdict(lambda: 0)
         for token in tokens:
             bow_dict[self.token2id(token)] += 1
         return bow_dict
-    
+
     def sklearn_bow(self, articles):
-        token_to_index_dict = {self.topic_model_dictionary.id2token[id]: index for id, index in self._word_to_top_word.items()}
+        token_to_index_dict = {
+            self.topic_model_dictionary.id2token[id]: index
+            for id, index in self._old_word_to_new_word.items()
+        }
         vectorizer = CountVectorizer(vocabulary=token_to_index_dict)
         return vectorizer.fit_transform(articles)
 
     def sklearn_bow_tensor(self, articles):
         return torch.tensor(self.sklearn_bow(articles).toarray())
-    
+
     def bow_tensor(self, tokens):
         bow_dict = self.bow_dict(tokens)
         bow_dict.pop(-1)
         word_indices = list(bow_dict.keys())
-        word_counts =  list(bow_dict.values())
+        word_counts = list(bow_dict.values())
         indices_tensor = torch.tensor(word_indices)
         counts_tensor = torch.tensor(word_counts)
-        return torch.sparse_coo_tensor(indices_tensor.view(1, -1), counts_tensor, size=(self.vocab_size,))
+        return torch.sparse_coo_tensor(
+            indices_tensor.view(1, -1), counts_tensor, size=(self.vocab_size,)
+        )
 
     def doc_2_ids(self, doc):
         return [self.token2id.get(token, -1) for token in doc.split()]
 
+    def new_tau_calc(self, tokens, with_oov=False):
+        W = self.word_topic_prob
+        T = np.mat(
+            [
+                topic_prob
+                for _, topic_prob in self.doc2topics(tokens)
+            ]
+        )
+        tau_d = T[:, self.old_relevant_topics] * W
+        if with_oov:
+            return np.append(tau_d, [[self._oov_value]], axis=1)
+        else:
+            return tau_d
+
     def tau_calc(self, tokens):
-        W = self.get_word_topic_prob()
-        relevant_topics = set(self._topic_to_top_topic.keys())
-        T = np.mat([topic_prob for topic_id, topic_prob in self.doc2topics(tokens) if topic_id in relevant_topics])
-        tau_d = T*W
+        W = self.word_topic_prob
+        relevant_topics = list(self._old_topic_to_new_topic.keys())
+        T = np.mat(
+            [
+                topic_prob
+                for topic_id, topic_prob in self.doc2topics(tokens)
+                if topic_id in relevant_topics
+            ]
+        )
+        tau_d = T * W
         return np.append(np.ravel(tau_d), self._oov_value)
--- test.py
+++ test.py
@@ -7,21 +7,26 @@ def get_token_weights(token_ids, token_weights, default_weight=1e-9):
     return [token_weights[0, token_id] if token_id != -1 else default_weight for token_id in token_ids]
 
 model = TopicModel(model_path='./testmodel/lda.model', dict_path='./testmodel/dictionary.dic', vocab_per_topic_size=1000, num_topics=100)
-model.get_word_topic_prob()
 id = model.token2id('lady')
-topic_id = model.get_word_topic_prob()[:, id].argmax()
-top_word_ids = np.argsort(-model.get_word_topic_prob()[topic_id, :])[:10]
+topic_id = model.word_topic_prob[:, id].argmax()
+top_word_ids = np.argsort(-model.word_topic_prob[topic_id, :])[:10]
 print([model.id2token(top_id) for top_id in top_word_ids])
-print(model.topic_model.show_topic(model._top_topic_to_topic[topic_id]))
+print(model.topic_model.show_topic(model._new_topic_to_old_topic[topic_id]))
 test_pd = read_test()
 test_article = test_pd.article.iloc[0]
 test_tokens = test_article.split()
-test_tau = model.tau_calc(test_article.split())
+test_tau, T = model.tau_calc(test_article.split())
+new_test_tau, new_T = model.new_tau_calc(test_article.split())
+print(test_tau == new_test_tau)
+print(T == new_T)
+print(T - new_T)
+test1 = model.doc2topics(test_article.split()) 
+test2 = model.doc2topics(test_article.split())
 from pprint import pprint
 #pprint(model._word_topic_prob)
 #print(test_tau)
 #print(model.bow_dict(test_tokens))
-tensor_numpy = model.bow_tensor(test_tokens).to_dense().numpy()
-sklearn_numpy = model.sklearn_bow([test_article]).toarray()
+# tensor_numpy = model.bow_tensor(test_tokens).to_dense().numpy()
+# sklearn_numpy = model.sklearn_bow([test_article]).toarray()
 print('done')
 #print(model.get_token_weights(test_tokens))
